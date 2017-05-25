1a6f248e1cf891ac08a6f0034e98b6c9103610db
Ivan Divito <divitoivan@gmail.com>
2017-05-25 20:07:28 -0300

    agrego codigo nrf51822 mbed

---
+++ codigo/cmake-build-debug/CMakeFiles/codigo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/codigo.dir/src/main.cpp.o"
+  "codigo.pdb"
+  "codigo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/codigo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
