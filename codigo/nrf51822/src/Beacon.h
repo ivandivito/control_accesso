52e428bc3a01665e25a925b493a24829937147b4
Ivan Divito <divitoivan@gmail.com>
2017-05-29 14:10:35 -0300

    add beacon serial com

--- codigo/nrf51822/src/Beacon.h
+++ codigo/nrf51822/src/Beacon.h
@@ -7,17 +7,18 @@
 
 
 #include "cmsis/arm_math.h"
+#include "string"
 
 class Beacon {
 
 private:
     uint16_t groupId;
     uint16_t unitId;
-    int32_t distanceRef;
-    double lastDistance;
+    uint8_t distanceRef;
+    uint8_t lastDistance;
 
 public:
-    Beacon(uint16_t groupId, uint16_t unitId, int32_t distanceRef) : groupId(groupId),
+    Beacon(uint16_t groupId, uint16_t unitId, uint8_t distanceRef) : groupId(groupId),
                                                                       unitId(unitId),
                                                                       distanceRef(distanceRef)
     {
@@ -36,25 +37,39 @@ public:
         return distanceRef;
     }
 
-    double getLastDistance() const {
+    uint8_t getLastDistance() const {
         return lastDistance;
     }
 
-    double calcDistance(int8_t rssi){
+    uint8_t calcDistance(int8_t rssi){
         if (rssi == 0) {
-            lastDistance = -1.0;
+            lastDistance = UINT8_MAX;
         } else {
             double ratio = ((double)rssi)/((double)distanceRef);
             if (ratio < 1.0) {
-                lastDistance = pow(ratio,10);
+                lastDistance = (uint8_t) pow(ratio, 10);
             }
             else {
-                lastDistance =  (0.89976)*pow(ratio,7.7095) + 0.111;
+                lastDistance = (uint8_t) ((0.89976) * pow(ratio, 7.7095) + 0.111);
             }
         }
         return lastDistance;
     }
 
+    void sentUpdate(Serial &pc){
+/*
+        pc.printf("%u ",(groupId & 0xFF00) >> 8);
+        pc.printf("%u ",groupId & 0x00FF);
+        pc.printf("%u ",(unitId & 0xFF00) >> 8);
+        pc.printf("%u ",unitId & 0x00FF);*/
+
+        pc.putc((groupId & 0xFF00) >> 8);
+        pc.putc(groupId & 0x00FF);
+        pc.putc((unitId & 0xFF00) >> 8);
+        pc.putc(unitId & 0x00FF);
+        pc.putc(lastDistance);
+        pc.putc('\n');
+    }
 
 
 };
--- codigo/nrf51822/src/main.cpp
+++ codigo/nrf51822/src/main.cpp
@@ -7,14 +7,31 @@
 #include "IBeaconAdvPackageData.h"
 #include "Beacon.h"
 
-EventQueue eventQueue(/* event count */ 16 * EVENTS_EVENT_SIZE);
+#define SERIAL_OPERATOR_CMD '$'
+#define SERIAL_OPERATOR_END 'E'
+#define SERIAL_OPERATOR_START 'S'
+#define SERIAL_OPERATOR_ASSIGN "="
+
+#define CMD_BUFFER_SIZE 30
+
+using namespace BleIBeacon;
+
+EventQueue eventQueue(/* cantidad de eventos */ 16 * EVENTS_EVENT_SIZE);
 
 DigitalOut led(P0_29, 1);
 Serial pc(P0_9,P0_11);
 
+bool sendUpdateEnable = false;
+char cmd_buffer[CMD_BUFFER_SIZE];
+int cmd_buffer_index = 0;
+
 void periodicCallback(void)
 {
-    led = !led; /* blink poncho led */
+    if(sendUpdateEnable){
+        led = !led; /* parpadeo led poncho si estoy enviando data*/
+    } else {
+        led = 0;
+    }
 }
 
 void advertisementCallback(const Gap::AdvertisementCallbackParams_t *params)
@@ -30,40 +47,42 @@ void advertisementCallback(const Gap::AdvertisementCallbackParams_t *params)
     pc.printf("\r\n");
 #endif
 
-    if(!BleIBeacon::isAdvertisingDatafromIBeacon(params->advertisingData,params->advertisingDataLen)){
-        return;
+    if(sendUpdateEnable){
+        if(!isAdvertisingDatafromIBeacon(params->advertisingData,params->advertisingDataLen)){
+            return;
+        }
+
+        IBeaconPayload_t *IBeaconPayload = (IBeaconPayload_t *) params->advertisingData;
+        uint16_t majorNumber = IBeaconPayload->majorNumberL | IBeaconPayload->majorNumberH << 8;
+        uint16_t minorNumber = IBeaconPayload->minorNumberL | IBeaconPayload->minorNumberH << 8;
+
+        #ifdef MAIN_DEBUG
+        pc.puts("beacon addr ");
+        pc.printf("largo: %d :",sizeof(params->peerAddr));
+        for (size_t i=0; i< sizeof(params->peerAddr); i++) {
+            printf("%02x ", params->peerAddr[i]);
+        }
+        pc.printf("rssi %d ", params->rssi);
+        pc.puts("UUID ");
+        pc.printf("largo: %d :",sizeof(IBeaconPayload->beaconUUID));
+        for (size_t i=0; i< sizeof(IBeaconPayload->beaconUUID); i++) {
+            printf("%02x ", IBeaconPayload->beaconUUID[i]);
+        }
+        pc.printf("major: %lu ", (unsigned long) majorNumber);
+        pc.printf("minor: %lu ", (unsigned long) minorNumber);
+        pc.printf("rssi ref: %d", IBeaconPayload->rssiRef);
+        pc.puts("\r\n");
+        #endif
+
+        Beacon beacon(majorNumber,minorNumber,(uint8_t)(256-IBeaconPayload->rssiRef));
+        beacon.calcDistance(-(params->rssi));
+
+        #ifdef MAIN_DEBUG
+        pc.printf("ref: %lu rssi: %d distance: %u \r\n", beacon.getDistanceRef(), -(params->rssi), beacon.getLastDistance());
+        #endif
+
+        beacon.sentUpdate(pc);
     }
-
-    BleIBeacon::IBeaconPayload_t *IBeaconPayload = (BleIBeacon::IBeaconPayload_t *) params->advertisingData;
-    uint16_t majorNumber = IBeaconPayload->majorNumberL | IBeaconPayload->majorNumberH << 8;
-    uint16_t minorNumber = IBeaconPayload->minorNumberL | IBeaconPayload->minorNumberH << 8;
-
-#ifdef MAIN_DEBUG
-    pc.puts("beacon addr ");
-    pc.printf("largo: %d :",sizeof(params->peerAddr));
-    for (size_t i=0; i< sizeof(params->peerAddr); i++) {
-        printf("%02x ", params->peerAddr[i]);
-    }
-    pc.printf("rssi %d ", params->rssi);
-    pc.puts("UUID ");
-    pc.printf("largo: %d :",sizeof(IBeaconPayload->beaconUUID));
-    for (size_t i=0; i< sizeof(IBeaconPayload->beaconUUID); i++) {
-        printf("%02x ", IBeaconPayload->beaconUUID[i]);
-    }
-    pc.printf("major: %lu ", (unsigned long) majorNumber);
-    pc.printf("minor: %lu ", (unsigned long) minorNumber);
-    pc.printf("rssi ref: %d", IBeaconPayload->rssiRef);
-    pc.puts("\r\n");
-#endif
-
-    Beacon beacon(majorNumber,minorNumber,256-IBeaconPayload->rssiRef);
-    double dist = beacon.calcDistance(-(params->rssi));
-
-#ifdef MAIN_DEBUG
-    pc.printf("ref: %lu rssi: %d distance: %f \r\n", beacon.getDistanceRef(), -(params->rssi), dist);
-#endif
-
-
 }
 
 void bleInitComplete(BLE::InitializationCompleteCallbackContext *params)
@@ -79,7 +98,7 @@ void bleInitComplete(BLE::InitializationCompleteCallbackContext *params)
         return;
     }
 
-    ble.gap().setScanParams(1800 /* scan interval */, 1500 /* scan window */);
+    ble.gap().setScanParams(1800 /* intervalo de scan */, 1500 /* ventana de scan */);
     ble.gap().startScan(advertisementCallback);
 }
 
@@ -88,10 +107,78 @@ void scheduleBleEventsProcessing(BLE::OnEventsToProcessCallbackContext* context)
     eventQueue.call(Callback<void()>(&ble, &BLE::processEvents));
 }
 
+void processCmd() {
+    if(cmd_buffer_index<=1 || cmd_buffer[0] != SERIAL_OPERATOR_CMD){
+        return;
+    }
+
+    if(isdigit(cmd_buffer[1])){
+/*
+        char buff[CMD_BUFFER_SIZE];
+        char buff_aux[10];
+        //busco si existe asignacion
+        int assignIndex = strcspn(cmd_buffer,SERIAL_OPERATOR_ASSIGN);
+        //busco el indice a modificar/leer
+        strncpy(buff,cmd_buffer + 1, (size_t) (assignIndex - 1));
+        buff[assignIndex-1] = '\0';
+        int index = atoi(buff);
+        if(assignIndex < cmd_buffer_index){
+
+            //si hay asignacion guardo
+            strncpy(buff_aux,cmd_buffer + assignIndex + 1, (size_t) (cmd_buffer_index - assignIndex));
+            buff_aux[cmd_buffer_index - assignIndex] = '\0';
+            int data = atoi(buff_aux);
+
+            switch (index){
+                case SERIAL_INDEX_:
+                    break;
+
+            }
+
+        }
+        //halla o no asignacion imprimo el valor
+        switch (index){
+            case SERIAL_INDEX_:
+                break;
+        }
+*/
+    } else {
+
+        switch (cmd_buffer[1]){
+            case SERIAL_OPERATOR_START:
+                sendUpdateEnable = true;
+                break;
+            case SERIAL_OPERATOR_END:
+                sendUpdateEnable = false;
+                break;
+        }
+
+    }
+
+}
+
+void rxCallback() {
+    char c = (char) pc.getc();
+    if(c == 0x0D || c == 0x0A){
+        cmd_buffer[cmd_buffer_index] = '\0';
+        processCmd();
+        cmd_buffer_index = 0;
+    } else {
+        cmd_buffer[cmd_buffer_index] = c;
+        cmd_buffer_index++;
+        if(cmd_buffer_index >= CMD_BUFFER_SIZE){
+            cmd_buffer_index = 0;
+        }
+    }
+}
+
 int main()
 {
     pc.baud(115200);
+    #ifdef MAIN_DEBUG
     pc.printf("init\r\n");
+    #endif
+    pc.attach((Callback<void()>)&rxCallback, Serial::RxIrq);
 
     eventQueue.call_every(1000, periodicCallback);
 
